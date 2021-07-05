405b0525945ed4b1a77924ec02ec9a3856493dd0
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-07-05 13:12:27 +0500

    cart comp implemented

---
+++ src/Item/Item.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@material-ui/core'
+import { CartItemType } from '../Types/Cart';
+
+// style
+import {  Wrapper } from './item.style';
+
+// types
+
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: (item:CartItemType) => void;
+}
+
+
+const Item : React.FC<Props>  = ({item, handleAddToCart}) => (
+    <Wrapper>
+        <img src={item.title} alt={item.title} />
+        <div>
+            <h3>
+                {item.title}
+            </h3>
+            <p>
+                {item.description}
+            </p>
+            <h3>
+                ${item.price}
+            </h3>
+        </div>
+        <Button onClick={() => handleAddToCart(item)}>
+            Add
+        </Button>
+    </Wrapper>
+)
